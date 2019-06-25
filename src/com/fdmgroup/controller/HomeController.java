d41a58fea1996c78330111602a943d52e484e9b3
mccarm416 <mccarm416@gmail.com>
2019-06-25 15:07:09 -0400

    Added user log in check

--- src/com/fdmgroup/controller/HomeController.java
+++ src/com/fdmgroup/controller/HomeController.java
@@ -20,20 +20,57 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public String showIndex(HttpSession session, @RequestParam String email, @RequestParam String password) {
 		System.out.println("-- HomeController --");
-		UserDao dao = new UserDao();
-		User foundUser = UserDao.findByEmail(email);
-		if (foundUser.getPassword().equals(password) ) {
+		User user = (User) session.getAttribute("user");
+		//Is there a logged in User
+		if (user != null) {
+			return redirectUser(user);
+		}
+		else if (email != null) {
+			//Does a user with matching credentials exist?
+			UserDao dao = new UserDao();
+			User foundUser = UserDao.findByEmail(email);
+			if (foundUser.getPassword().equals(password) ) {
+				//User logs in and is added to session
+				session.setAttribute("user", foundUser);
+				return redirectUser(foundUser);
+			}
+				
+			
+			else {
+				//User does not login
+				return "index";
+			}
 			
 		}
-		User user = (User) session.getAttribute("user");
+
 
 			
 		
 		return "index";
 	}
 
-	private static void redirectUser() {
-		
+	private static String redirectUser(User user) {
+		if (user != null) {
+			if (user.getClass() == Trainee.class) {
+				//Forward to job postings
+				System.out.println("Trainee signed in");
+				return "redirect:/jobPostings";
+			}
+			else if (user.getClass() == AccountManager.class) {
+				//Forward to AM dashboard
+				System.out.println("AM signed in");
+				return "redirect:/accountManagerDashboard";
+			}
+			else if (user.getClass() == SystemAdministrator.class) {
+				//Forward to SystemAdmin dashboard
+				System.out.println("SysAdmin signed in");
+				return "redirect:/sysAdminDashboard";
+			}
+			else if (user.getClass() == SalesAdministrator.class) {
+				//Forward to SalesAdmin dashboard
+				System.out.println("SalesAdmin signed in");
+				return "redirect:/salesAdminDashboard";
+			}
 	}
 
 }
