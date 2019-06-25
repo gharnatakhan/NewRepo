beb9b8f6fb42d8d556577d815d1663c172deb21a
luckyP86H <Paul.Xu@fdmgroup.com>
2019-06-25 15:10:41 -0400

    Added logout function

--- src/com/fdmgroup/controller/HomeController.java
+++ src/com/fdmgroup/controller/HomeController.java
@@ -1,6 +1,10 @@
 package com.fdmgroup.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +13,18 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public String showIndex() {
 		return "index";
+		
 	}
+
+    @RequestMapping(value="/logout")
+    public String logout(HttpServletRequest req, ModelMap model) {
+           System.out.println("logout()");
+         HttpSession session = req.getSession();
+         session.invalidate();
+         model.addAttribute("logoutMsg", "successfully logged-out");
+         
+         return "index";
+
+    }
+
 }
