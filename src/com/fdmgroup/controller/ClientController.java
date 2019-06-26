5c87fbdc60723354192d1abbd9271e745df9dd32
Nithin Venkat <Nithin.Venkat@fdmgroup.com>
2019-06-26 15:50:24 -0400

    Cleaned up ClientController files

--- src/com/fdmgroup/controller/ClientController.java
+++ src/com/fdmgroup/controller/ClientController.java
@@ -1,24 +1,38 @@
 package com.fdmgroup.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.fdmgroup.dao.ClientDao;
 import com.fdmgroup.model.Client;
 import com.fdmgroup.model.User;
 
 @Controller
 public class ClientController {
 
-	@RequestMapping("/clients")
-	public String showAllClients(HttpSession session, Client client) {
-		User user = (User) session.getAttribute("client");
-		if (user != empty) {
+	@RequestMapping(value = "/clients", method = RequestMethod.POST)
+	public String showAllClients(HttpSession session, Model model) {
+		User user = (User) session.getAttribute("user");
+		if (user == null) {
+
+			System.out.println("Inside showAllClients");
 			// get all the clients and store in the list of string
-			List<String> listOfClients = clientDao.findAll();
+			List<Client> listOfClients = new ArrayList<Client>();
+			ClientDao cDao = new ClientDao();
+
+			// Need to change after the confirmation from dao
+			listOfClients = cDao.findAll();
+			System.out.println(listOfClients.size());
+			model.addAttribute("AllClient", listOfClients);
+			System.out.println("Exiting showAllClient..");
 		}
+		return "index";
 	}
 }
