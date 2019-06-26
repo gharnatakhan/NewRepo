7d1a979ed33467178ba1f89e13e880358709bccb
Anurag Das <Anurag.Das@fdmgroup.com>
2019-06-26 09:37:06 -0400

    Reworked on testing app and created small scale mock db

--- src/com/fdmgroup/app/TestingApp.java
+++ src/com/fdmgroup/app/TestingApp.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import com.fdmgroup.dao.UserDao;
 import com.fdmgroup.model.AccountManager;
 
 import com.fdmgroup.model.Trainee;
@@ -11,25 +12,22 @@ import com.fdmgroup.model.Trainee;
 public class TestingApp {
 
 	public static void main(String[] args) {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("cauldron");
-		EntityManager em = emf.createEntityManager();
 		
 		
+		UserDao userDao = new UserDao();
 		
-		em.getTransaction().begin();
+		
 		
 		Trainee trainee = new Trainee("trainee", "lastname", "@", "kys", "path", "pwd");
-		em.persist(trainee);
+		
 		
 		AccountManager accountmanager = new AccountManager("firstname", "last", "email", "ee", "path", "pwd");
-		em.persist(accountmanager);
-		
+		userDao.create(trainee);
+		userDao.create(accountmanager);
 		System.out.println("Data stored");
 		
-		em.getTransaction().commit();
-		em.close();
-		emf.close();
-
+	
+	
 	}
 	
 }
