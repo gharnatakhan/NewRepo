a29fe32e915eaee89ad2d841394fc5fb82af20ac
Nithin Venkat <Nithin.Venkat@fdmgroup.com>
2019-06-27 14:51:46 -0400

    Implemented (1.01) change placement status

--- src/com/fdmgroup/controller/SalesAdminController.java
+++ src/com/fdmgroup/controller/SalesAdminController.java
@@ -3,16 +3,20 @@ package com.fdmgroup.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
+import org.omg.CORBA.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fdmgroup.dao.ApplicationDao;
+import com.fdmgroup.dao.TraineeDao;
 import com.fdmgroup.dao.UserDao;
 import com.fdmgroup.model.Application;
 import com.fdmgroup.model.Trainee;
@@ -43,5 +47,29 @@ public class SalesAdminController {
 		
 		return "applicationHistory";
 	}
+	
+	// Change a trainee placement status
+	@RequestMapping(value="/ModifyTraineeStatus")
+		public String ChangeTraineeStatus(@PathVariable int userid ,@RequestParam("statusForm") String new_status, HttpSession session, Model model) {
+			
+			System.out.println("Entering change status method...");
+			User user = (User) session.getAttribute("user");
+			
+			//Fetch the trainee using userid from User table
+			UserDao userDao = new UserDao();
+			User foundUser = userDao.findById(userid);
+			
+			//Update the employment status
+			TraineeDao tdao = new TraineeDao();
+			Trainee trainee_obj = tdao.findById(foundUser.getUserId());
+			tdao.update(trainee_obj, new_status);
+					
+			model.addAttribute("StatusUpdated",trainee_obj);
+			
+			//
+			return "index";
+		}
+	
+	
 
 }
